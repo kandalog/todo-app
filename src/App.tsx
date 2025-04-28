7e059d89d1e8ae5e3bca2562e3f4cc7613bb6d74
kandalog <asuasu.tee@gmail.com>
2025-04-29 01:41:47 +0900

    jotaiの導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { Button } from "./components/ui/button";
-
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Button>ボタン</Button>
     </>
   );
 }
