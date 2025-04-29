04e1be22d1d2fec81fbbf07c1813d0d5ea25f1fb
kandalog <asuasu.tee@gmail.com>
2025-04-30 02:43:05 +0900

    編集と削除ボタンを追加

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -3,9 +3,27 @@ import { Todo } from "@/domain/Todo";
 type Props = Todo;
 
 function TodoItem(todo: Props) {
+  const handleEdit = () => {
+    console.log("編集 Click!");
+  };
+
+  const handleDelete = () => {
+    console.log("削除 click!");
+  };
+
   return (
-    <div className="box">
-      <p>{todo.title}</p>
+    <div className="border">
+      <div className="flex">
+        <p>{todo.title}</p>
+        <div>
+          <button className="border cursor-pointer" onClick={handleEdit}>
+            編集
+          </button>
+          <button className="ml-2 border cursor-pointer" onClick={handleDelete}>
+            削除
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
